439fa860c2a8540d7967f8a5c9fb60f5aba570d6
Aleksandar Todorov <linked2u@gmail.com>
2019-01-28 19:40:13 +0200

    Code refactoring

--- tfe-cli-py_app/tfe-cli-py.py
+++ tfe-cli-py_app/tfe-cli-py.py
@@ -2,21 +2,24 @@
 
 import urllib.request, json
 
-#token = "Bearer " + str(input('Please enter your token: '))
-token = "Bearer " + 
-#organization_name = str(input('Please enter your organization name: '))
+token = "Bearer " + str(input('Please enter your token: '))
+organization_name = str(input('Please enter your organization name: '))
+
+"""
+# The following lines are left for development purposes. Should be removed in
+# the final version
+
+
+token = "Bearer " + ''
 organization_name = 'HiveCorp'
-#workspace_name = str(input('Please enter your worksapce name: '))
+workspace_name = str(input('Please enter your worksapce name: '))
+"""
 
 # Define the main url to TFE
 url = 'https://app.terraform.io/api/v2/'
 
-#rget = requests.get
-
-# Variable containing the ID of your workspace
-#wspace_id_url = str(url + 'organizations/' + organization_name + '/workspaces/' + workspace_name)
+# Compose URL from which worskpaces should be obtained.
 org_workspaces_url = str(url + 'organizations/' + organization_name + '/workspaces/')
-#payload={'Organization': 'HiveCorp', 'workspace': 'ExtIP'}
 
 #Define the headers to be used
 
@@ -24,11 +27,11 @@ header_1 = {'key' : 'Authorization', 'value' : token }
 header_2 = {'key' : 'Content-Type', 'value' : 'application/vnd.api+json'}
 
 
-#print(org_workspaces_url)
-# Create an URL to request all workspaces within the given Organization
+# Create a request to obtain all workspaces for a given organization (output
+# depends on the permissions your user has)
 wspaces_request = urllib.request.Request(org_workspaces_url)
 
-# Attach headers to the URL created
+# Attach headers to the prevuously created request
 wspaces_request.add_header(header_1['key'], header_1['value'])
 wspaces_request.add_header(header_2['key'], header_2['value'])
 
@@ -40,14 +43,12 @@ with urllib.request.urlopen(wspaces_request) as connection:
 
 # Count how many workspaces are available
 workspaces_count = str(org_wspaces_inf).count('name')
+
+# Create a list with the available workspaces
+
 workspaces_list = []
 for i in range(workspaces_count):
     workspaces_list.append(json.loads(org_wspaces_inf)['data'][i]['attributes']['name'])
-
-#print(workspaces_list)
-
-#for workspace in workspaces_list:
-#    print(workspace)
 
 def return_menu_option(options_list):
     """
@@ -56,27 +57,44 @@ def return_menu_option(options_list):
     Returns the value of the option chosen by the user.
 
     >>> return_menu_option(['apple', 'pear' 'banana'])
+    [1] apple
+    [2] pear
+    [3] banana
+    Please choose an option by entering a number between 1 and 3 or 'quit' to exit: 1
+
+    >>> return_menu_option(['apple', 'pear' 'banana'])
+    [1] apple
+    [2] pear
+    [3] banana
+    Please choose an option by entering a number between 1 and 3 or 'quit' to exit: quit
+    Exiting...
 
     """
-    for i in range(len(options_list)):
-        for option in options_list:
-            print('[{0}]'.format(i), option)
-        break
+    for i in range(1, len(options_list) + 1):
+        print('[{0}]'.format(i), options_list[i - 1])
     choice = 'empty'
-    while lower.choice != 'quit':
+    while choice.lower() != 'quit':
         choice = input(str("Please choose an option by entering a number between 1 and {0} or 'quit' to exit: ".format(len(options_list))))
-        if choice.lower == 'quit':
+        if choice.lower() == "quit":
             print('Exiting...')
-        elif int(choice) in range(len(options_list)):
-            for i in range(len(options_list)):
+            break
+        elif len(choice) > 1:
+            print("Invalid option!")
+            print("Try again.")
+            continue
+        elif int(choice) in range(1, len(options_list) + 1):
+            for i in range(1, len(options_list) + 1):
                 if int(choice) == i:
-                    return options_list[i]
+                    return options_list[i-1]
                 else:
                     continue
 
         else:
             print("Invalid option.")
             print("Try again...")
-            continue
 
+# The following code is left for debugging purposes. Should be removed in the
+# final version. 
 selected_workspace = return_menu_option(workspaces_list)
+
+print(selected_workspace, type(selected_workspace))
