f5281a4fc4d0b83591195dd28244a39bc135c0df
Alexander Todorov <alext@Alexanders-MacBook-Pro.local>
2019-01-28 17:45:06 +0200

    tfe-cli-py.py

--- tfe-cli-py_app/tfe-cli-py.py
+++ tfe-cli-py_app/tfe-cli-py.py
@@ -1,25 +1,82 @@
 #!/usr/bin/env python3
 
-import requests, urllib3
+import urllib.request, json
 
-token = "Bearer " + str(input('Please enter your token: '))
-organization_name = str(input('Please enter your organization name: '))
-workspace_name = str(input('Please enter your worksapce name: '))
+#token = "Bearer " + str(input('Please enter your token: '))
+token = "Bearer " + 'AyyxEpg871u6wA.atlasv1.Qlnaaez3Zrv7qmTyyPa0I5bQGpbiiQmZxcZfez8lrjx7ZaorQkyijruJ6hmUMnUqyCs'
+#organization_name = str(input('Please enter your organization name: '))
+organization_name = 'HiveCorp'
+#workspace_name = str(input('Please enter your worksapce name: '))
+
 # Define the main url to TFE
 url = 'https://app.terraform.io/api/v2/'
 
-rget = requests.get
+#rget = requests.get
+
 # Variable containing the ID of your workspace
-wspace_id_url = str(url + 'organizations/' + organization_name + '/workspaces/' + workspace_name)
-
-
+#wspace_id_url = str(url + 'organizations/' + organization_name + '/workspaces/' + workspace_name)
+org_workspaces_url = str(url + 'organizations/' + organization_name + '/workspaces/')
 #payload={'Organization': 'HiveCorp', 'workspace': 'ExtIP'}
+
 #Define the headers to be used
-headers = {'Authorization': token , 'Content-Type': 'application/vnd.api+json'}
-wspace_id = rget( wspace_id_url , headers=headers)
 
-print(wspace_id_url)
-print(wspace_id.status_code)
+header_1 = {'key' : 'Authorization', 'value' : token }
+header_2 = {'key' : 'Content-Type', 'value' : 'application/vnd.api+json'}
 
-print(wspace_id.text)
-print(wspace_id.url)
+
+#print(org_workspaces_url)
+# Create an URL to request all workspaces within the given Organization
+wspaces_request = urllib.request.Request(org_workspaces_url)
+
+# Attach headers to the URL created
+wspaces_request.add_header(header_1['key'], header_1['value'])
+wspaces_request.add_header(header_2['key'], header_2['value'])
+
+
+# Gather workspaces information
+with urllib.request.urlopen(wspaces_request) as connection:
+   org_wspaces_inf =  connection.read()
+
+
+# Count how many workspaces are available
+workspaces_count = str(org_wspaces_inf).count('name')
+workspaces_list = []
+for i in range(workspaces_count):
+    workspaces_list.append(json.loads(org_wspaces_inf)['data'][i]['attributes']['name'])
+
+#print(workspaces_list)
+
+#for workspace in workspaces_list:
+#    print(workspace)
+
+def return_menu_option(options_list):
+    """
+    (list) -> str
+    A function that creates a menu from a list.
+    Returns the value of the option chosen by the user.
+
+    >>> return_menu_option(['apple', 'pear' 'banana'])
+
+    """
+    for i in range(len(options_list)):
+        for option in options_list:
+            print('[{0}]'.format(i), option)
+        break
+    choice = 'empty'
+    while lower.choice != 'quit':
+        choice = input(str("Please choose an option by entering a number between 1 and {0} or 'quit' to exit: ".format(len(options_list))))
+        if choice.lower == 'quit':
+            print('Exiting...')
+        elif int(choice) in range(len(options_list)):
+            for i in range(len(options_list)):
+                if int(choice) == i:
+                    return options_list[i]
+                else:
+                    continue
+
+        else:
+            print("Invalid option.")
+            print("Try again...")
+            continue
+
+selected_workspace = return_menu_option(workspaces_list)
